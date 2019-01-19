8420a0dd5b53a408cdf17d613d18aa0e12e5b98f
matejgrcic <matej.grcic@axilis.com>
2019-01-19 22:32:24 +0100

    labeled dataset added

--- load_datasets.m
+++ load_datasets.m
@@ -1,3 +1,3 @@
-images = load_images('images', ".jpg", "Screenshot_");
+images = load_images('images', ".jpg", "example_");
 [labels, label_map] = create_label_map('labels.txt');
 [glcm_dataset, fractal_dataset, tiocm_dataset] = prepare_datasets(images);
