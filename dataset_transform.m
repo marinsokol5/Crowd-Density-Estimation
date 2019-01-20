33255e0a1b7faa76d13657b4e4a9f7c558faed95
matejgrcic <matej.grcic@axilis.com>
2019-01-20 08:19:03 +0100

    train test split added

--- dataset_transform.m
+++ dataset_transform.m
@@ -3,13 +3,14 @@ for l = 1: 60
    l_svm(l) = labels{1, l};
    fractal_svm(l) = fractal_dataset{1, l};
 end
+l_svm = l_svm'
+fractal_svm = fractal_svm'
 tiocm_svm = zeros(60, 6);
 for l = 1: 60
     for j = 1: 6
         tiocm_svm(l, j) = tiocm_dataset{1, l}(j);
     end
 end
-
 glcm_svm = zeros(60,16 * 1521);
 for l = 1: 60
     for j = 1: 16
