d7bc618f8b05a3cdc75e58c5d0393b6fecadf039
Luka Banović <luka.banovic@fer.hr>
2019-01-21 16:48:08 +0100

    fixed fractal dimension

--- fractalDimension.m
+++ fractalDimension.m
@@ -1,5 +1,5 @@
 function [fractalDimension] = fractalDimension(edges)
-    imageToProcess = (edges < 0.5);
+    imageToProcess = (edges < 150);
     [n,r] = boxcount(imageToProcess);
     df = -diff(log(n))./diff(log(r));
     fractalDimension = mean(df(4:8));
